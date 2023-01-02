2b19b3e3dc6738e5094f30d10499af95c1d929e7
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-01-02 23:37:34 +0100

    use zeitgeist mainnet in example

--- packages/example/src/sdk/index.ts
+++ packages/example/src/sdk/index.ts
@@ -1,5 +1,5 @@
 import * as Ztg from "@zeitgeistpm/indexer";
 
 export const zeitgeist = Ztg.create({
-  uri: "https://processor.bsr.zeitgeist.pm/graphql",
+  uri: "https://processor.rpc-0.zeitgeist.pm/graphql",
 });
